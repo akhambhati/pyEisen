af4ac57612c4b52418eb502d7f6e5dff57356d3f
Ankit Khambhati <akhambhati@gmail.com>
2023-08-23 22:35:56 -0700

    Incorporate header

--- src/pyeisen/__init__.py
+++ src/pyeisen/__init__.py
@@ -1,1 +1,12 @@
 """PyEisen."""
+
+from . import header
+
+
+__author__ = header.__author__
+__email__ = header.__email__
+
+__license__ = header.__license__
+
+__version__ = header.__version__
+__status__ = header.__status__
