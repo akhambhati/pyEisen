0191d2864d6e9314cd9fa02c2583fcb11c1af73c
Ankit Khambhati <akhambhati@gmail.com>
2023-08-23 22:35:40 -0700

    Added typing definitions

--- src/pyeisen/family.py
+++ src/pyeisen/family.py
@@ -1,18 +1,37 @@
-"""
-Functions and Wrappers to define families of kernels for signal analysis.
+"""Functions and Wrappers to define families of kernels for signal analysis.
 
 Author: Ankit N. Khambhati
 Adapted from: https://github.com/pennmem/ptsa_new/blob/master/ptsa/wavelet.py
 Last Updated: 2023/08/23
 """
 
+from typing import Any
+from typing import Dict
+from typing import Tuple
+from typing import TypedDict
+
 import numpy as np
+import numpy.typing as npt
 from scipy.signal import morlet as scipy_morlet
 
 
-def morlet(freqs, cycles, Fs, n_win=7, complete=True):
-    """
-    Calculate Morlet wavelets with the total energy normalized to 1.
+class Family(TypedDict):
+    """Defines the wavelet family and parameters used to construct kernels."""
+
+    kernel: npt.NDArray[np.complex_]
+    params: Dict[str, Any]
+    sample: Dict[str, npt.NDArray[np.float_]]
+    axis_ord: Tuple[str, str]
+
+
+def morlet(
+    freqs: npt.NDArray[np.float_],
+    cycles: npt.NDArray[np.int_],
+    fs: float,
+    n_win: int = 7,
+    complete: bool = True,
+) -> Family:
+    """Calculate Morlet wavelets with the total energy normalized to 1.
 
     Calls the scipy.signal.wavelet.morlet() function to generate
     Morlet wavelets with the specified frequencies, samplerates, and
@@ -26,7 +45,7 @@ def morlet(freqs, cycles, Fs, n_win=7, complete=True):
     cycles: np.ndarray, shape (n_wavelet, dtype=float)
         The number of cycles for each wavelet.
         Needs to be same size as freqs.
-    Fs: float
+    fs: float
         The sampling frequency (e.g. 200 Hz) of the signal to which wavelet
         will be applied.
     n_win: float, (default=7)
@@ -45,25 +64,23 @@ def morlet(freqs, cycles, Fs, n_win=7, complete=True):
         A family of Morlet wavelets equal in number to the frequency/cycle
         pairs provided. Each wavelet entry spans same length but diff. decay.
     """
-
     # Check Inputs:
     if len(freqs) < 1:
-        raise ValueError('At least one frequency must be specified.')
+        raise ValueError("At least one frequency must be specified.")
     if len(cycles) != len(freqs):
-        raise ValueError(
-            'Each frequency must have an associated number of cycles.')
-    if Fs <= 0:
-        raise ValueError('Sampling frequency must be non-negative.')
+        raise ValueError("Each frequency must have an associated number of cycles.")
+    if fs <= 0:
+        raise ValueError("Sampling frequency must be non-negative.")
 
     # Temporal standard deviation of the wavelet (ratio of cycles to frequency)
     st = cycles / (2 * np.pi * freqs)
 
     # Support length for the wavelet (length in samples)
-    max_len = int(np.max(np.ceil(st * Fs * n_win)))
+    max_len = int(np.max(np.ceil(st * fs * n_win)))
 
     # Scaling factor for the wavelet.
     # Depends on frequency, cycles, support length, and sampling frequency.
-    scales = (freqs * max_len) / (2 * cycles * Fs)
+    scales = (freqs * max_len) / (2 * cycles * fs)
 
     # generate list of unnormalized wavelets:
     family = [
@@ -72,29 +89,24 @@ def morlet(freqs, cycles, Fs, n_win=7, complete=True):
     ]
 
     # generate list of energies for the wavelets:
-    energies = [np.sqrt(np.sum(np.abs(wavelet)**2)) for wavelet in family]
+    energies = [np.sqrt(np.sum(np.abs(wavelet) ** 2)) for wavelet in family]
 
     # normalize the wavelets by dividing each one by its energy:
-    family = [family[i] / energies[i] for i in range(len(family))]
-
-    # Convert to an array bank
-    family = np.array(family)
+    kernels = np.array([family[i] / energies[i] for i in range(len(family))])
 
     # Sort all vals to the scale
     scales = 1 / scales
     scale_ord = np.argsort(scales)
 
-    wavelet_family = {
-        'kernel': family[scale_ord],
-        'wavelet': {
-            'scales': scales[scale_ord],
-            'freqs': freqs[scale_ord],
-            'cycles': cycles[scale_ord]
+    wavelet_family: Family = {
+        "kernel": kernels[scale_ord],
+        "params": {
+            "scales": scales[scale_ord],
+            "freqs": freqs[scale_ord],
+            "cycles": cycles[scale_ord],
         },
-        'sample': {
-            'time': np.arange(max_len) / Fs
-        },
-        'axis_ord': np.array(['wavelet', 'sample'])
+        "sample": {"time": np.arange(max_len) / fs},
+        "axis_ord": ("wavelet", "sample"),
     }
 
     return wavelet_family
