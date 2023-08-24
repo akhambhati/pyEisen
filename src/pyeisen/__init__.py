92434329d65307d0b6e33df8e0f920404ee28cfa
Ankit Khambhati <akhambhati@gmail.com>
2023-08-23 23:18:24 -0700

    Remove header tracking, this info is in package metadata

--- src/pyeisen/__init__.py
+++ src/pyeisen/__init__.py
@@ -1,12 +1,1 @@
 """PyEisen."""
-
-from . import header
-
-
-__author__ = header.__author__
-__email__ = header.__email__
-
-__license__ = header.__license__
-
-__version__ = header.__version__
-__status__ = header.__status__
